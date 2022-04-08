8d51afb45558fd4e08e5f988b89d918d393d7851
21ulwe <21ulwe@stockholmscience.se>
2022-04-08 18:28:52 +0200

    Minor bugg fixes

--- VulcanStocksKNNResearchMVVM/ViewModels/ShellViewModel.cs
+++ VulcanStocksKNNResearchMVVM/ViewModels/ShellViewModel.cs
@@ -3,12 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using Caliburn.Micro;
 
 namespace VulcanStocksKNNResearchMVVM.ViewModels
 {
     public class ShellViewModel : Conductor<object>.Collection.AllActive
     {
+        public ShellViewModel()
+        {
+            
+        }
+
+        public void Logo_Click()
+        {
+
+        }
+
+        public void Demotrade_Click()
+        {
+
+        }
+
+        public void Manual_Click()
+        {
+
+        }
+
+        public void FBE_Click()
+        {
+
+        }
+
+        public void Settings_Click()
+        {
+
+        }
+
+        public void Close_Click()
+        {
+            System.Environment.Exit(0);
+        }
+
+
 
     }
 }
