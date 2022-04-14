d22d848230f1595870679456c58a44aaee957a8e
21ulwe <21ulwe@stockholmscience.se>
2022-04-14 18:30:56 +0200

    -

--- VulcanStocksKNNResearchMVVM/Models/StrategyWriter.cs
+++ VulcanStocksKNNResearchMVVM/Models/StrategyWriter.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace VulcanStocksKNNResearchMVVM.Models
 {
     public class StrategyWriter
     {
+        private string dataPath;
+        private string[][] dataSet;
+        private string[] data { get; set; }
+
+        public void Write(string ticker)
+        {
+            dataPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "\\StockData\\" + ticker + ".csv";
+            Setup();
+
+        }
+
+        private void Setup()
+        {
+            data = File.ReadAllLines(dataPath);
+
+            dataSet = new string[data.Length][];
+            for (int i = 0; i < data.Length; i++)
+            {
+                string[] temp = data[i].Split(',');
+                dataSet[i] = new string[temp.Length];
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    dataSet[i][j] = temp[j];
+                }
+            }
+        }
+
+
     }
 }
